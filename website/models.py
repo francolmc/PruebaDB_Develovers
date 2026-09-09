28545fb0cd942429adde7b3928d8c01e50c5559f
Franco Morales Castro <franco.morales@outlook.com>
2026-09-09 08:42:18 -0300

    Agregamos el modelo Categoria y lo registramos en admin; Actualizamos el modelo de Proyecto para incluir la relacion con Categoria

--- website/models.py
+++ website/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
 class Proyecto(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
     tecnologia = models.CharField(max_length=100)
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.CASCADE, 
+        related_name='proyectos',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.titulo
