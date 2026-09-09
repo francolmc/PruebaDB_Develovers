407d4ddaa82e7d0dc8da6b3f424f4e3db617b5e2
Franco Morales Castro <franco.morales@outlook.com>
2026-09-09 09:15:30 -0300

    Agregamos la tabla Tecnologia y actualizamos Proyecto para usar una relacion de mucho a mucho con Tecnologia

--- website/models.py
+++ website/models.py
@@ -6,17 +6,27 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Tecnologia(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
     
 class Proyecto(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
-    tecnologia = models.CharField(max_length=100)
     categoria = models.ForeignKey(
         Categoria, 
         on_delete=models.CASCADE, 
         related_name='proyectos',
         null=True,
         blank=True
+    )
+    tecnologias = models.ManyToManyField(
+        Tecnologia,
+        related_name='proyectos',
+        blank=True        
     )
 
     def __str__(self):
